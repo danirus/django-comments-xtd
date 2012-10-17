28a3c87a2b5b1e60b48b6bffb435d1826a7186fd
Daniel Rus Morales <danirus@eml.cc>
2012-10-17 21:48:31 +0200

    Bump version in setup.py file

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ test.run_tests = run_tests
 
 setup(
     name = "django-comments-xtd",
-    version = "1.0a5",
+    version = "1.1a1",
     packages = find_packages(),
     include_package_data = True,
     license = "MIT",
-    description = "Django Comments Framework extension app with follow up notifications and email confirmations",
-    long_description = "A reusable Django app that extends the built-in Django's Comments Framework with following up notifications and comments that only hits the database after users confirm them by email.",
+    description = "Django Comments Framework extension app with thread support, follow up notifications and email confirmations.",
+    long_description = "A reusable Django app that extends the built-in Django's Comments Framework with thread support, following up notifications and comments that only hits the database after users confirm them by email.",
     author = "Daniel Rus Morales",
     author_email = "inbox@danir.us",
     maintainer = "Daniel Rus Morales",

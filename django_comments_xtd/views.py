2a3b93bc1ab6bbc5146664770f31e288fbf28506
Daniel Rus Morales <danirus@eml.cc>
2012-11-08 13:14:29 +0100

    Search for the 'posted.html' template at different paths

--- django_comments_xtd/views.py
+++ django_comments_xtd/views.py
@@ -89,7 +89,16 @@ def sent(request):
         comment_pk = int(comment_pk)
         comment = XtdComment.objects.get(pk=comment_pk)
     except (TypeError, ValueError, XtdComment.DoesNotExist):
-        return render_to_response("comments/posted.html", 
+        template_arg = [
+            "django_comments_xtd/%s/%s/posted.html" % (
+                comment.content_type.app_label, 
+                comment.content_type.model),
+            "django_comments_xtd/%s/posted.html" % (
+                comment.content_type.app_label,),
+            "django_comments_xtd/posted.html",
+            "comments/posted.html"
+        ]
+        return render_to_response(template_arg, 
                                   context_instance=RequestContext(request))
     else:
         if request.is_ajax() and comment.user and comment.user.is_authenticated():

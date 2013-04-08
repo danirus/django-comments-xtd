a0d91cc509e6cfb71776898270adb680a66e9c2b
Martin Mahner <martin@mahner.org>
2013-04-08 17:14:28 +0200

    Create and pass a dict instead of a Comment instance before saving.
    This fixes a bug coming with (probably) Django 1.5. Traceback was:
    
    Exception Type: TypeError at /forum/post/
    Exception Value: ModelBase object argument after ** must be a mapping, not Comment

--- django_comments_xtd/views.py
+++ django_comments_xtd/views.py
@@ -9,7 +9,7 @@ from django.template import loader, Context, RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django_comments_xtd import signals, signed
 from django_comments_xtd.conf import settings
-from django_comments_xtd.models import (XtdComment, TmpXtdComment, 
+from django_comments_xtd.models import (XtdComment, TmpXtdComment,
                                         max_thread_level_for_content_type)
 from django_comments_xtd.utils import send_mail
 
@@ -18,8 +18,8 @@ def send_email_confirmation_request(comment, target, key, text_template="django_
     """Send email requesting comment confirmation"""
     subject = _("comment confirmation request")
     confirmation_url = reverse("comments-xtd-confirm", args=[key])
-    message_context = Context({ 'comment': comment, 
-                                'content_object': target, 
+    message_context = Context({ 'comment': comment,
+                                'content_object': target,
                                 'confirmation_url': confirmation_url,
                                 'contact': settings.DEFAULT_FROM_EMAIL,
                                 'site': Site.objects.get_current() })
@@ -38,7 +38,7 @@ def _comment_exists(comment):
     True if exists a XtdComment with same user_name, user_email and submit_date.
     """
     return (XtdComment.objects.filter(
-            user_name=comment.user_name, 
+            user_name=comment.user_name,
             user_email=comment.user_email,
             submit_date=comment.submit_date).count() > 0)
 
@@ -47,25 +47,30 @@ def _create_comment(tmp_comment):
     """
     Creates a XtdComment from a TmpXtdComment.
     """
-    comment = XtdComment(**tmp_comment)
+    # Get all data out of the comment object as a dict. Don't take over keys
+    # starting with an underscore.
+    comment = {}
+    for k, v in tmp_comment.__dict__.iteritems():
+        if not k.startswith('_'):
+            comment[k] = v
+    comment = XtdComment(**comment)
     comment.is_public = True
     comment.save()
     return comment
 
-
 def on_comment_was_posted(sender, comment, request, **kwargs):
     """
     Post the comment if a user is authenticated or send a confirmation email.
-    
-    On signal django.contrib.comments.signals.comment_was_posted check if the 
-    user is authenticated or if settings.COMMENTS_XTD_CONFIRM_EMAIL is False. 
+
+    On signal django.contrib.comments.signals.comment_was_posted check if the
+    user is authenticated or if settings.COMMENTS_XTD_CONFIRM_EMAIL is False.
     In both cases will post the comment. Otherwise will send a confirmation
     email to the person who posted the comment.
     """
     if settings.COMMENTS_APP != "django_comments_xtd":
         return False
 
-    if (not settings.COMMENTS_XTD_CONFIRM_EMAIL or 
+    if (not settings.COMMENTS_XTD_CONFIRM_EMAIL or
         (comment.user and comment.user.is_authenticated())):
         if not _comment_exists(comment):
             new_comment = _create_comment(comment)
@@ -76,7 +81,7 @@ def on_comment_was_posted(sender, comment, request, **kwargs):
         object_pk = request.POST["object_pk"]
         model = models.get_model(*ctype.split("."))
         target = model._default_manager.get(pk=object_pk)
-        key = signed.dumps(comment, compress=True, 
+        key = signed.dumps(comment, compress=True,
                            extra_key=settings.COMMENTS_XTD_SALT)
         send_email_confirmation_request(comment, target, key)
 
@@ -91,13 +96,13 @@ def sent(request):
     except (TypeError, ValueError, XtdComment.DoesNotExist):
         template_arg = ["django_comments_xtd/posted.html",
                         "comments/posted.html"]
-        return render_to_response(template_arg, 
+        return render_to_response(template_arg,
                                   context_instance=RequestContext(request))
     else:
         if request.is_ajax() and comment.user and comment.user.is_authenticated():
             template_arg = [
                 "django_comments_xtd/%s/%s/comment.html" % (
-                    comment.content_type.app_label, 
+                    comment.content_type.app_label,
                     comment.content_type.model),
                 "django_comments_xtd/%s/comment.html" % (
                     comment.content_type.app_label,),
@@ -128,7 +133,7 @@ def confirm(request, key, template_discarded="django_comments_xtd/discarded.html
     # Check whether a signal receiver decides to discard the contact_msg
     for (receiver, response) in responses:
         if response == False:
-            return render_to_response(template_discarded, 
+            return render_to_response(template_discarded,
                                       {'comment': tmp_comment},
                                       context_instance=RequestContext(request))
 
@@ -141,7 +146,7 @@ def notify_comment_followers(comment):
     followers = {}
 
     previous_comments = XtdComment.objects.filter(
-        object_pk=comment.object_pk, is_public=True, 
+        object_pk=comment.object_pk, is_public=True,
         followup=True).exclude(id__exact=comment.id)
 
     for instance in previous_comments:
@@ -156,8 +161,8 @@ def notify_comment_followers(comment):
 
     for email, name in followers.iteritems():
         message_context = Context({ 'user_name': name,
-                                    'comment': comment, 
-                                    'content_object': target, 
+                                    'comment': comment,
+                                    'content_object': target,
                                     'site': Site.objects.get_current() })
         text_message = text_message_template.render(message_context)
         html_message = html_message_template.render(message_context)
@@ -172,7 +177,7 @@ def reply(request, cid):
 
     if comment.level == max_thread_level_for_content_type(comment.content_type):
         return render_to_response(
-            "django_comments_xtd/max_thread_level.html", 
+            "django_comments_xtd/max_thread_level.html",
             {'max_level': settings.COMMENTS_XTD_MAX_THREAD_LEVEL},
             context_instance=RequestContext(request))
 
@@ -181,12 +186,12 @@ def reply(request, cid):
 
     template_arg = [
         "django_comments_xtd/%s/%s/reply.html" % (
-            comment.content_type.app_label, 
+            comment.content_type.app_label,
             comment.content_type.model),
         "django_comments_xtd/%s/reply.html" % (
             comment.content_type.app_label,),
         "django_comments_xtd/reply.html"
     ]
-    return render_to_response(template_arg, 
+    return render_to_response(template_arg,
                               {"comment": comment, "form": form, "next": next },
                               context_instance=RequestContext(request))

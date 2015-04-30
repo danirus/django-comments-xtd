2aed14fd38123750bb30c2bac72b253cf08ef976
Daniel Rus Morales <danirus@eml.cc>
2015-04-30 13:35:40 +0200

    Update travis script

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test.run_tests = run_tests
 
 setup(
     name = "django-comments-xtd",
-    version = "1.4b1",
+    version = "1.4b2",
     packages = find_packages(),
     include_package_data = True,
     license = "MIT",

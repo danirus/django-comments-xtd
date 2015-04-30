9ee1eaedc6e7581d18de41792bccd01c08c8a7a0
Daniel Rus Morales <danirus@eml.cc>
2015-04-30 13:09:53 +0200

    Update version for PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test.run_tests = run_tests
 
 setup(
     name = "django-comments-xtd",
-    version = "1.3a1",
+    version = "1.4b1",
     packages = find_packages(),
     include_package_data = True,
     license = "MIT",

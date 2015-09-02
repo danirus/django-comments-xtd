3dc5dfe197dfc31fffa1f6b4d717cdc77dc7dec4
Daniel Rus Morales <danirus@eml.cc>
2015-09-02 12:09:27 +0200

    Release 1.4.0 final and upload it to PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test.run_tests = run_tests
 
 setup(
     name = "django-comments-xtd",
-    version = "1.4b3",
+    version = "1.4.0",
     packages = find_packages(),
     include_package_data = True,
     license = "MIT",

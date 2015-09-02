04231b1678be23d003933f5e0b64de385a34f305
Daniel Rus Morales <danirus@eml.cc>
2015-09-02 12:00:44 +0200

    Bump version number

--- django_comments_xtd/__init__.py
+++ django_comments_xtd/__init__.py
@@ -33,7 +33,7 @@ def get_model():
 def get_form():
     return import_string(settings.COMMENTS_XTD_FORM_CLASS)
 
-VERSION = (1, 4, 0, 'b', 2) # following PEP 440
+VERSION = (1, 4, 0, 'b', 3) # following PEP 440
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])

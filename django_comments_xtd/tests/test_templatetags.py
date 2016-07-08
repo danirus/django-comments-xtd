89ea92e2010a957478d838f95c510585cae9b012
Daniel Rus Morales <danirus@eml.cc>
2016-07-08 15:55:32 +0200

    pep8 compliant

--- django_comments_xtd/tests/test_templatetags.py
+++ django_comments_xtd/tests/test_templatetags.py
@@ -1,4 +1,3 @@
-#-*- coding: utf-8 -*-
 try:
     from unittest.mock import patch
 except ImportError:
@@ -8,59 +7,65 @@ import unittest
 from django.template import TemplateSyntaxError
 from django.test import TestCase as DjangoTestCase
 
-from django_comments_xtd.templatetags.comments_xtd import render_markup_comment, formatter
+from django_comments_xtd.templatetags.comments_xtd import (
+    render_markup_comment, formatter)
 
 
-@unittest.skipIf(not formatter, "This test case needs django-markup, docutils and markdown installed to be run")
+@unittest.skipIf(not formatter, "This test case needs django-markup, "
+                 "docutils and markdown installed to be run")
 class RenderMarkupValueFilterTestCase(DjangoTestCase):
 
     def test_render_markup_comment_in_markdown(self):
-        comment = r'''#!markdown
-An [example](http://url.com/ "Title")'''
+        comment = '#!markdown\nAn [example](http://url.com/ "Title")'
         result = render_markup_comment(comment)
         self.assertEqual(result,
-                         '<p>An <a href="http://url.com/" title="Title">example</a></p>')
+                         '<p>An <a href="http://url.com/" title="Title">'
+                         'example</a></p>')
 
     def test_render_markup_comment_in_restructuredtext(self):
-        comment = r'''#!restructuredtext
-A fibonacci generator in Python, taken from `LiteratePrograms <http://en.literateprograms.org/Fibonacci_numbers_%28Python%29>`_::
-
-    def fib():
-        a, b = 0, 1
-        while 1:
-            yield a
-            a, b = b, a + b'''
+        comment = ('#!restructuredtext\n'
+                   'A fibonacci generator in Python, taken from '
+                   '`LiteratePrograms <http://en.literateprograms.org/'
+                   'Fibonacci_numbers_%28Python%29>`_::\n\n'
+                   '    def fib():\n'
+                   '        a, b = 0, 1\n'
+                   '        while 1:\n'
+                   '            yield a\n'
+                   '            a, b = b, a + b')
         result = render_markup_comment(comment)
         self.assertEqual(result,
-                         r'''<div class="document">
-<p>A fibonacci generator in Python, taken from <a class="reference external" href="http://en.literateprograms.org/Fibonacci_numbers_%28Python%29">LiteratePrograms</a>:</p>
-<pre class="literal-block">
-def fib():
-    a, b = 0, 1
-    while 1:
-        yield a
-        a, b = b, a + b
-</pre>
-</div>
-''')
+                         ('<div class="document">\n'
+                          '<p>A fibonacci generator in Python, taken from '
+                          '<a class="reference external" href="http://en.'
+                          'literateprograms.org/Fibonacci_numbers_%28'
+                          'Python%29">LiteratePrograms</a>:</p>\n'
+                          '<pre class="literal-block">\n'
+                          'def fib():\n'
+                          '    a, b = 0, 1\n'
+                          '    while 1:\n'
+                          '        yield a\n'
+                          '        a, b = b, a + b\n'
+                          '</pre>\n'
+                          '</div>\n'))
 
 
-@unittest.skipIf(not formatter, "This test case needs django-markup, docutils and markdown installed to be run")
+@unittest.skipIf(not formatter, "This test case needs django-markup, "
+                 "docutils and markdown installed to be run")
 class RenderFallbackMarkupValueFilterTestCase(DjangoTestCase):
 
     @patch.multiple('django_comments_xtd.conf.settings',
                     COMMENTS_XTD_MARKUP_FALLBACK_FILTER='markdown')
-    def test_render_fallback_markup_comment(self):
+    def test_render_fallback_markup_comment_markdown(self):
         with patch.multiple('django_comments_xtd.conf.settings',
                             COMMENTS_XTD_MARKUP_FALLBACK_FILTER='markdown'):
             comment = r'''An [example](http://url.com/ "Title")'''
             result = render_markup_comment(comment)
-            self.assertEqual(result,
-                             '<p>An <a href="http://url.com/" title="Title">example</a></p>')
+            self.assertEqual(result, '<p>An <a href="http://url.com/" '
+                             'title="Title">example</a></p>')
 
     @patch.multiple('django_comments_xtd.conf.settings',
                     COMMENTS_XTD_MARKUP_FALLBACK_FILTER=None)
-    def test_render_fallback_markup_comment(self):
+    def test_render_fallback_no_markup_comment(self):
         with patch.multiple('django_comments_xtd.conf.settings',
                             COMMENTS_XTD_MARKUP_FALLBACK_FILTER=None):
             comment = r'''An [example](http://url.com/ "Title")'''
@@ -69,7 +74,8 @@ class RenderFallbackMarkupValueFilterTestCase(DjangoTestCase):
                              r'''An [example](http://url.com/ "Title")''')
 
 
-@unittest.skipIf(formatter, "This test case needs django-markup or docutils or markdown not installed to be run")
+@unittest.skipIf(formatter, "This test case needs django-markup or docutils "
+                 "or markdown not installed to be run")
 class MarkupNotAvailableTestCase(DjangoTestCase):
 
     def test_render_markup_comment(self):

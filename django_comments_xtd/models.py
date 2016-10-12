5149cffed5824aaa72668de41dde969021777a28
Daniel Rus Morales <danirus@eml.cc>
2016-10-12 15:16:39 +0200

    Fix pep8 complaints

--- django_comments_xtd/models.py
+++ django_comments_xtd/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.db.models import F, Max, Min
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext, ugettext_lazy as _
-from django.utils.encoding import force_text
 
 try:
     from django.db.transaction import atomic
@@ -44,9 +43,7 @@ class MaxThreadLevelExceededException(Exception):
                 {"max_thread_level": self.max_by_app})
 
 
-# class XtdCommentManager(models.Manager):
 class XtdCommentManager(CommentManager):
-
     if django.VERSION[:2] < (1, 6):
         get_queryset = models.Manager.get_query_set
 
@@ -63,17 +60,6 @@ class XtdCommentManager(CommentManager):
         qs = self.get_queryset().filter(content_type__in=content_types)\
                                 .reverse()
         return qs
-
-    # def for_model(self, model):
-    #     """
-    #     QuerySet for all comments for a particular model (either an instance or
-    #     a class).
-    #     """
-    #     ct = ContentType.objects.get_for_model(model)
-    #     qs = self.get_queryset().filter(content_type=ct)
-    #     if isinstance(model, models.Model):
-    #         qs = qs.filter(object_pk=force_text(model._get_pk_val()))
-    #         return qs
 
 
 class XtdComment(Comment):

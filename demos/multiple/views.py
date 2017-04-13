53614cfa5aeaa9814eac287e9ab2498e2099ae52
Daniel Rus Morales <danirus@eml.cc>
2017-04-13 10:00:54 +0200

    Remove demos dir, example projects are in example dir

--- demos/multiple/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response as render
-from django.template import RequestContext
-
-
-def homepage_v(request):
-    return render("homepage.html", context_instance=RequestContext(request))
-
-def contact_v(request):
-    return render("contact.html", context_instance=RequestContext(request))

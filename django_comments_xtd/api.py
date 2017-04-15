82d7537444e1be9b9c5f176474c96bb4b032d632
Daniel Rus Morales <danirus@eml.cc>
2017-04-15 22:24:45 +0200

    XtdCommentSerializer, basic views, no authentication

--- django_comments_xtd/api.py
+++ django_comments_xtd/api.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.models import ContentType
 from django.http import Http404
 
 from rest_framework import generics
@@ -8,9 +9,26 @@ from django_comments_xtd.serializers import XtdCommentSerializer
 
 class XtdCommentList(generics.ListCreateAPIView):
     """List all comments or create a new comment."""
-    queryset = XtdComment.objects.all()
     serializer_class = XtdCommentSerializer
 
+    def get_queryset(self):
+        content_type_arg = self.kwargs.get('content_type', None)
+        object_pk_arg = self.kwargs.get('object_pk', None)
+        app_label, model = content_type_arg.split("-")
+        content_type = ContentType.objects.get_by_natural_key(app_label, model)
+        qs = XtdComment.objects.filter(content_type=content_type,
+                                       object_pk=int(object_pk_arg),
+                                       is_public=True,
+                                       is_removed=False)
+        return qs
+
+    def perform_create(self, serializer):
+        content_type_arg = self.kwargs.get('content_type', None)
+        object_pk_arg = self.kwargs.get('object_pk', None)
+        app_label, model = content_type_arg.split("-")
+        content_type = ContentType.objects.get_by_natural_key(app_label, model)
+        serializer.save(content_type=content_type, object_pk=int(object_pk_arg))
+        
 
 class XtdCommentDetail(generics.RetrieveUpdateAPIView):
     """Retrieve or update a comment instance."""
--- django_comments_xtd/serializers.py
+++ django_comments_xtd/serializers.py
@@ -3,10 +3,13 @@ from django_comments_xtd.models import XtdComment
 
 
 class XtdCommentSerializer(serializers.ModelSerializer):
+    submit_date = serializers.DateTimeField(read_only=True)
+    thread_id = serializers.IntegerField(read_only=True)
+    level = serializers.IntegerField(read_only=True)
+    order = serializers.IntegerField(read_only=True)
+    
     class Meta:
         model = XtdComment
-        fields = ('id', 'content_type', 'object_pk', 'site', 'user',
-                  'user_name', 'user_email', 'user_url', 'comment',
-                  'submit_date', 'is_public', 'is_removed', 'thread_id',
-                  'parent_id', 'level', 'order', 'followup')
-        
+        fields = ('id', 'site', 'user', 'user_name', 'user_email', 'user_url',
+                  'comment', 'submit_date', 'thread_id', 'parent_id', 'level',
+                  'order', 'followup')

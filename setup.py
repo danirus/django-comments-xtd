e63208ee78defe5fb3caad7e73bab07cf07369b8
Daniel Rus Morales <danirus@eml.cc>
2017-05-05 15:32:01 +0200

    Include static dir in MANIFEST.in file

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test.run_tests = run_tests
 
 setup(
     name="django-comments-xtd",
-    version="1.7.1",
+    version="1.7.2",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",

cde3c227338aa3a80fcaf5075c52a6bd8ab388fb
Daniel Rus Morales <danirus@eml.cc>
2017-06-09 23:54:18 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test.run_tests = run_tests
 
 setup(
     name="django-comments-xtd",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",

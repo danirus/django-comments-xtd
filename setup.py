af140c61c87261e02ff05944b73782e8fbd56114
Daniel Rus Morales <danirus@sciclist.com>
2020-01-26 10:38:13 +0100

    Release 2.4.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test.run_tests = run_tests
 
 setup(
     name="django-comments-xtd",
-    version="2.4.2",
+    version="2.4.3",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         plugin: path.resolve(SOURCE_DIR, 'index.js')
     },
     output: {
-        filename: '[name]-2.4.2.js',
+        filename: '[name]-2.4.3.js',
         path: STATIC_DIR
     },
     optimization: {

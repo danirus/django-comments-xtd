f3396098431a1c9e0bfe7caaadfcba1413a6d507
Daniel Rus Morales <danirus@eml.cc>
2021-02-28 15:45:37 +0100

    Release version 2.8.4.

--- django_comments_xtd/__init__.py
+++ django_comments_xtd/__init__.py
@@ -18,7 +18,7 @@ def get_form():
     return import_string(settings.COMMENTS_XTD_FORM_CLASS)
 
 
-VERSION = (2, 8, 3, 'f', 0)  # following PEP 440
+VERSION = (2, 8, 4, 'f', 0)  # following PEP 440
 
 
 def get_version():

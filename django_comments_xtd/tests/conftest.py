e99568bd1da80aa565bb714d9f58a198b13d6e08
Daniel Rus Morales <danirus@eml.cc>
2021-06-26 07:44:52 +0200

    Let conftest.py define DJANGO_SETTINGS_MODULE

--- django_comments_xtd/tests/conftest.py
+++ django_comments_xtd/tests/conftest.py
@@ -2,15 +2,10 @@ import os
 import sys
 
 
-def setup_django_settings():
+def pytest_configure(config):
     os.chdir(os.path.join(os.path.dirname(__file__), ".."))
     sys.path.insert(0, os.getcwd())
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
-
-
-def pytest_configure(config):
-    if not os.environ.get("DJANGO_SETTINGS_MODULE", False):
-        setup_django_settings()
 
     import django
     django.setup()

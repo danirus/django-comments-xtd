42c7efae6a75d118806f13065bb9efb3df537eaf
Ihor Marhitych <imarhitych@ripe.net>
2021-08-13 14:42:19 +0200

    remove unused imports

--- django_comments_xtd/api/views.py
+++ django_comments_xtd/api/views.py
@@ -1,21 +1,16 @@
-import six
-
 from django.db.models import F
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
-from django.utils.module_loading import import_string
 
 from django_comments_xtd import get_model as get_comment_model
 from django_comments.views.moderation import perform_flag
 from rest_framework import generics, mixins, permissions, status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.schemas.openapi import AutoSchema
 
 from django_comments_xtd.conf import settings
 from django_comments_xtd.api import serializers
-from django_comments_xtd.models import (TmpXtdComment, XtdComment,
-                                        CommentReaction)
+from django_comments_xtd.models import XtdComment, CommentReaction
 from django_comments_xtd.utils import check_option, get_current_site_id
 
 

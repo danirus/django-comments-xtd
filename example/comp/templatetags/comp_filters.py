a1f8cdfb8e172dc0a18b80d1bfe459b2d0ff2155
Khoding <julien.rutscho@ceff.ch>
2021-12-07 10:36:46 +0100

    ugettext_lazy > gettext_lazy (for Django 4.0)

--- example/comp/templatetags/comp_filters.py
+++ example/comp/templatetags/comp_filters.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.template import Library, TemplateSyntaxError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from django.conf import settings
 
--- example/custom/mycomments/admin.py
+++ example/custom/mycomments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from django_comments_xtd.admin import XtdCommentsAdmin
 from custom.mycomments.models import MyComment

d4e47654a92f294daa0d506f05dbc873365d41d4
Daniela Rus Morales <danirus@eml.cc>
2023-06-29 09:26:54 +0200

    Split reactjs Comment component into smaller components

--- django_comments_xtd/static/django_comments_xtd/js/mocks/django.js
+++ django_comments_xtd/static/django_comments_xtd/js/mocks/django.js
@@ -1,6 +1,20 @@
 function _django() {
   return {
-    gettext: (arg) => arg
+    gettext: (arg) => arg,
+    ngettext: (singular, plural, count) => {
+      return (count == 1) ? singular : plural;
+    },
+    interpolate: (fmt, obj, named) => {
+      return named
+        ? fmt.replace(
+          /%\(\w+\)s/g,
+          function (match) { return String(obj[match.slice(2,-2)]) }
+        )
+        : fmt.replace(
+          /%s/g,
+          function(match){ return String(obj.shift()) }
+        )
+    }
   }
 };
 

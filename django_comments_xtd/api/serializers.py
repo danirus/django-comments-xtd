94368cf5db1d0dcc3c13d7c2aa596803f5e67846
Daniela Rus Morales <danirus@eml.cc>
2023-12-22 17:37:02 +0100

    Add COMMENTS_XTD_API_DATETIME_FORMAT to defaults.py

--- django_comments_xtd/api/serializers.py
+++ django_comments_xtd/api/serializers.py
@@ -24,6 +24,10 @@ from django_comments_xtd.utils import get_app_model_options
 
 COMMENT_MAX_LENGTH = getattr(settings, 'COMMENT_MAX_LENGTH', 3000)
 
+COMMENTS_XTD_API_DATETIME_FORMAT = getattr(
+    settings, 'COMMENTS_XTD_API_DATETIME_FORMAT', settings.DATETIME_FORMAT
+)
+
 
 class WriteCommentSerializer(serializers.Serializer):
     content_type = serializers.CharField()
@@ -274,9 +278,7 @@ class ReadCommentSerializer(serializers.ModelSerializer):
         else:
             submit_date = obj.submit_date
         return formats.date_format(
-            submit_date,
-            settings.COMMENTS_XTD_API_DATETIME_FORMAT,
-            use_l10n=True
+            submit_date, COMMENTS_XTD_API_DATETIME_FORMAT, use_l10n=True
         )
 
     def get_comment(self, obj):

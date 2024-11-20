67b86b8342403617a0e1200a4b6213193ed533f7
Niall French <niall@circuitmind.io>
2024-11-20 12:02:41 +0000

    Test with Django 5.1, increase Django range to 5.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test.run_tests = run_tests
 
 setup(
     name="django-comments-xtd",
-    version="2.10.0",
+    version="2.10.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
@@ -35,7 +35,7 @@ setup(
     maintainer_email="danirus@eml.cc",
     url="http://pypi.python.org/pypi/django-comments-xtd",
     install_requires=[
-        'Django>=3,<5.1',
+        'Django>=3,<5.2',
         'django-contrib-comments>=2.2,<2.3',
         'djangorestframework>=3.12,<3.16',
         'docutils',

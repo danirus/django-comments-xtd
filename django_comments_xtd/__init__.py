4bbe08b21b29d86e42b598d3809cda3ba8e029f3
Daniela Rus Morales <157103+danirus@users.noreply.github.com>
2025-10-11 10:27:04 +0200

    Prepare release 2.10.10 (#477)

--- django_comments_xtd/__init__.py
+++ django_comments_xtd/__init__.py
@@ -17,7 +17,7 @@ def get_form():
     return import_string(settings.COMMENTS_XTD_FORM_CLASS)
 
 
-VERSION = (2, 10, 9, "f", 0)  # following PEP 440
+VERSION = (2, 10, 10, "f", 0)  # following PEP 440
 
 
 def get_version():
